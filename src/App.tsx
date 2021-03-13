c403fac769d317b96f01377e9307fa851222d06d
Andrey Sysoev <andrey@sysoev.ru>
2021-03-13 11:44:19 +0300

    redirect works

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import queryString from 'query-string';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import SecondPage from './components/SecondPage';
 import StartPage from './components/StartPage';
 
 function App() {
+  const [keyword, setKeyword] = useState('');
+
+  useEffect(() => {
+    if (keyword) {
+      window.location.href = `/results?${queryString.stringify({ keyword })}`;
+    }
+  }, [keyword]);
+
   return (
     <div className="App">
-      <StartPage />
-      {/* <SecondPage /> */}
+      <BrowserRouter>
+        <Route exact path="/">
+          <Redirect to="/search" />
+        </Route>
+        <Switch>
+          <Route
+            exact
+            path="/search"
+            component={() => <StartPage onSetKeyword={setKeyword} />}
+          />
+          <Route exact path="/results" component={SecondPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/StartPage/index.tsx
+++ src/components/StartPage/index.tsx
@@ -1,25 +1,43 @@
+import React, { useState } from 'react';
 import './StartPage.scss';
 
-const StartPage = () => (
-  <div className="start-page">
-    <header className="header">
-      <p className="header__logo">Lumberjacks</p>
-    </header>
-    <main className="main">
-      <h1 className="main__title">Поиск товаров</h1>
-      <p className="main__subtitle">
-        Выбирайте товар мечты из лучших предложений
-      </p>
-      <form className="main__form">
-        <input
-          type="text"
-          className="main__input"
-          placeholder="Введите название товара"
-        />
-        <button className="main__button">Найти</button>
-      </form>
-    </main>
-  </div>
-);
+const StartPage = ({
+  onSetKeyword,
+}: {
+  onSetKeyword: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    onSetKeyword(inputValue);
+    setInputValue('');
+  };
+
+  return (
+    <div className="start-page">
+      <header className="header">
+        <p className="header__logo">Lumberjacks</p>
+      </header>
+      <main className="main">
+        <h1 className="main__title">Поиск товаров</h1>
+        <p className="main__subtitle">
+          Выбирайте товар мечты из лучших предложений.
+        </p>
+        <form className="main__form" onSubmit={(e) => onFormSubmit(e)}>
+          <input
+            type="text"
+            className="main__input"
+            placeholder="Введите название товара"
+            value={inputValue}
+            onChange={({ target: { value } }) => setInputValue(value)}
+          />
+          <button className="main__button">Найти</button>
+        </form>
+      </main>
+    </div>
+  );
+};
 
 export default StartPage;
